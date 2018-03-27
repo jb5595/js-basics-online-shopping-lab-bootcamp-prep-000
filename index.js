af4971dd9830bd0723e5815e2a581bd609191553
Jacob Berman <jacobberman1995@gmail.com>
2018-03-27 20:41:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -82,7 +82,7 @@ function placeOrder(cardNumber) {
     return "Sorry, we don't have a credit card on file for you.";
   }
   else{ 
-    total = cart.total();
+    total = total();
     cart = [];
     return `Your total cost is $${total}, which will be charged to the card ${cardNumber}`;
 }
