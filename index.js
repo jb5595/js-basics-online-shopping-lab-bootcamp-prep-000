192a96bb588f4758445a04a8824103ed508bab65
Jacob Berman <jacobberman1995@gmail.com>
2018-03-27 20:28:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -62,6 +62,7 @@ function total() {
 function removeFromCart(item) {
   // write your code here
     var itemcheck = 0;
+    return cart;
   for (let i = 0; i < cart.length; i ++){
     if (cart[i].itemName == item){
       cart.splice(i);
