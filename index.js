fda46edc330046ee38fd5ad9bb1a5dc466809fcc
Jacob Berman <jacobberman1995@gmail.com>
2018-03-27 20:36:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -78,5 +78,7 @@ function removeFromCart(item) {
 
 function placeOrder(cardNumber) {
   // write your code here
-  if 
+  if (cardNumber == undefined){
+    return "Sorry, we don't have a credit card on file for you."
+  }
 }
