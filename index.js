3dfeba76ae0db5bfadc395e3a2928975d569a74a
Jacob Berman <jacobberman1995@gmail.com>
2018-03-27 20:41:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -84,7 +84,7 @@ function placeOrder(cardNumber) {
   else{ 
     total = cart.total();
     cart = {}
-    return `Your total cost is $${total()}, which will be charged to the card ${cardNumber}`;
+    return `Your total cost is $${total}, which will be charged to the card ${cardNumber}`;
 }
 }
 
